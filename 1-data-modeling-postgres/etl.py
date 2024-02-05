8cae527016b09e05531a5828cafb68450f7f0691
Yana P. <79352236+Yanadade@users.noreply.github.com>
2024-02-05 11:33:54 +0000

    insert actor

--- 1-data-modeling-postgres/etl.py
+++ 1-data-modeling-postgres/etl.py
@@ -2,9 +2,13 @@ import glob
 import json
 import os
 from typing import List
+#import logging
+#from datetime import datetime
 
 import psycopg2
 
+# Set up logging
+#logging.basicConfig(level=logging.WARNING)
 
 def get_files(filepath: str) -> List[str]:
     """
@@ -27,6 +31,9 @@ def process(cur, conn, filepath):
     # Get list of files from filepath
     all_files = get_files(filepath)
 
+    # get datetime
+    #curr_dt = datetime.now()
+
     for datafile in all_files:
         with open(datafile, "r") as f:
             data = json.loads(f.read())
@@ -39,6 +46,10 @@ def process(cur, conn, filepath):
                         each["type"],
                         each["actor"]["id"],
                         each["actor"]["login"],
+                        each["actor"]["display_login"],
+                        each["actor"]["gravatar_id"],
+                        each["actor"]["url"],
+                        each["actor"]["avatar_url"],
                         each["repo"]["id"],
                         each["repo"]["name"],
                         each["created_at"],
@@ -50,6 +61,10 @@ def process(cur, conn, filepath):
                         each["type"],
                         each["actor"]["id"],
                         each["actor"]["login"],
+                        each["actor"]["display_login"],
+                        each["actor"]["gravatar_id"],
+                        each["actor"]["url"],
+                        each["actor"]["avatar_url"],
                         each["repo"]["id"],
                         each["repo"]["name"],
                         each["created_at"],
@@ -58,25 +73,34 @@ def process(cur, conn, filepath):
                 # Insert data into tables here
                 insert_statement = f"""
                     INSERT INTO actors (
-                        id,
-                        login
-                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
-                    ON CONFLICT (id) DO NOTHING
+                        actor_id, 
+                        actor_login, 
+                        actor_display_login, 
+                        actor_gravatar_id, 
+                        actor_url, 
+                        actor_avatar_url
+                ) VALUES (  '{each["actor"]["id"]}', 
+                            '{each["actor"]["login"]}',
+                            '{each["actor"]["display_login"]}',
+                            '{each["actor"]["gravatar_id"]}',
+                            '{each["actor"]["url"]}',
+                            '{each["actor"]["avatar_url"]}')
+                    ON CONFLICT (actor_id) DO NOTHING
                 """
                 # print(insert_statement)
                 cur.execute(insert_statement)
 
                 # Insert data into tables here
-                insert_statement = f"""
-                    INSERT INTO events (
-                        id,
-                        type,
-                        actor_id
-                    ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
-                    ON CONFLICT (id) DO NOTHING
-                """
-                # print(insert_statement)
-                cur.execute(insert_statement)
+                # insert_statement = f"""
+                #     INSERT INTO events (
+                #         id,
+                #         type,
+                #         actor_id
+                #     ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
+                #     ON CONFLICT (id) DO NOTHING
+                # """
+                # # print(insert_statement)
+                # cur.execute(insert_statement)
 
                 conn.commit()
 
@@ -85,11 +109,14 @@ def main():
     conn = psycopg2.connect(
         "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
     )
+    #logging.warning('connected postgresdb')
     cur = conn.cursor()
 
     process(cur, conn, filepath="../data")
+    #logging.warning('wrote to db')
 
     conn.close()
+    #logging.warning('disconnect postgresdb--closed')
 
 
 if __name__ == "__main__":
